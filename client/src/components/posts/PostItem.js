d679aea9ff78dd4729eb9f2fb07a9fa78bff3932
samuel wondimu <wsamuelwondimu@gmail.com>
2021-03-21 01:08:38 +0300

    fix post text length in sinplge post and multiple post route

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import formatDate from '../../utils/formatDate';
 import { connect } from 'react-redux';
 import { addLike, removeLike, deletePost } from '../../actions/post';
+
+const Route = () => {
+  const location = useLocation();
+  return location.pathname;
+};
 
 const PostItem = ({
   addLike,
@@ -21,12 +27,21 @@ const PostItem = ({
           <h4 style={{ color: '#333' }}>{name}</h4>
         </Link>
       </div>
+
       <div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: text.substring(0, 400).trim() + '...'
-          }}
-        ></div>
+        {Route() === '/posts' ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: text.substring(0, 400).trim() + '...'
+            }}
+          ></div>
+        ) : (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: text
+            }}
+          ></div>
+        )}
 
         <p className="post-date">Posted on {formatDate(date)}</p>
         {showActions && (
