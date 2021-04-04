c2899c1465e6802111223d1c96f7f83c89462ce8
samuelwondimu <wsamuelwondimu@gmail.com>
2021-04-04 13:59:58 +0300

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import store from '../store';
 import { LOGOUT } from '../actions/types';
 
 const api = axios.create({
-  baseURL: 'http://inchina-api.herokuapp.com/api',
+  baseURL: 'https://inchina-api.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
